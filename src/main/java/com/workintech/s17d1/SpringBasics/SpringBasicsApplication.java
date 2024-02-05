02d49aadb60fde7eff4efb7b558112ddf8758f04
hmzshin <hamzasahin@outlook.com>
2024-02-05 14:22:11 +0300

    Complete tasks

---
+++ src/main/java/com/workintech/s17d1/SpringBasics/SpringBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.s17d1.SpringBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsApplication.class, args);
+	}
+
+}
